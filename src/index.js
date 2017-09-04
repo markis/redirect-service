40d6d0458f2f9278080ff29a90fedcdce72df1ec
Markis Taylor <markis@users.noreply.github.com>
2017-09-04 15:24:46 -0700

    Add football-graphql

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const urls = {
     'code-coverage':        'https://marketplace.visualstudio.com/items?itemName=markis.code-coverage',
     'football':             'https://football.markis.codes',
+    'football-graphql':     'https://football-graphql.herokuapp.com',
     'newtab':               'https://chrome.google.com/webstore/detail/newtab/lokmlhkeameimafbgfmkempkfmhcphpa',
     'trutographer':         'http://trulia-innovation.s3-website-us-west-2.amazonaws.com/',
     'optimizing-for-gzip':  'http://s.codepen.io/markis/debug/dvdJbo/vPMKKVwXKGQM'
